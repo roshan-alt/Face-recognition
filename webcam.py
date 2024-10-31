a499398a51e164616b57642e1e180caf5dc376e1
roshan-alt <roshanadhikari0420@gmail.com>
2024-10-31 20:05:28 +0545

    Add files via upload
    "Initial Commit"

---
+++ webcam.py
@@ -0,0 +1,92 @@
+'''
+THIS IS A FACE RECOGNITION APPLICATION THIS USES FACE RECOGNITION MODULE BUT IT IS JUST A TRAILER.
+I AM LEARNING MACHINE LEARNING  AND DEEP LEARNING AFTER THAT I WILL USE MY OWN FACE RECOGNITION SYSYTEM FROM SCRATCH.
+'''
+
+
+
+#IMPORTING MODULES
+import cv2
+import numpy as np
+import face_recognition
+import os
+from threading import Thread
+
+# ACCESSING IMAGES  FROM FOLDER AND READING THE IMAGE.
+path = 'images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    print(cl)
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+
+def findEncodings(images):
+    """
+    ENCODE THE IMAGE THAT IS IN THE FOLDER AND RETURN THE LIST.
+    """
+    encodeList = []
+
+
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+
+### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+# def captureScreen(bbox=(300,300,690+300,530+300)):
+#     capScr = np.array(ImageGrab.grab(bbox))
+#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#     return capScr
+
+
+encodeListKnown = findEncodings(images)
+# print(encodeListKnown)
+print('Encoding Complete')
+
+
+
+def face_recognize():
+    """
+    USE ALL THINGS FROM ABOVE FOLDER AND RECOGNIZE THE FACE.
+    """
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, img = cap.read()
+    # img = captureScreen()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+    # print(name)
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
+
+face_recognize_thread = Thread(target=face_recognize)
+face_recognize_thread.start()
